e670be74bc63a057a8dfd960791d1c8d4e70dc3c
Jock Lawrie <jlaw1812@hre.azure>
2020-06-22 23:32:05 +0000

    Experimenting with JSON3 for saveload module.

--- src/saveload.jl
+++ src/saveload.jl
@@ -3,13 +3,22 @@ module saveload
 export save, load
 
 using Dates
-using JSON
+#using JSON
+using JSON3
 
 using ..persons
 using ..contacts.households
 using ..contacts.workplaces
 using ..contacts.social_networks
 using ..contacts.community_networks
+
+
+JSON3.StructTypes.StructType(::Type{Person{A, S}}) where {A, S} = JSON3.StructTypes.Mutable()
+JSON3.StructTypes.StructType(::Type{households.Household})      = JSON3.StructTypes.Mutable()
+
+Person{A, S}() where {A, S} = Person(0, Date(1900, 1, 1), 'o', 'x', nothing, 0, nothing, nothing, 0, 0)
+households.Household() = households.Household(0, 0, Int[], Int[])
+
 
 function save(people::Vector{Person{A, S}}, filename::String) where {A, S}
     d = Dict{String, Any}()
@@ -18,13 +27,15 @@ function save(people::Vector{Person{A, S}}, filename::String) where {A, S}
     d["workplaces"]        = workplaces._workplaces
     d["communitycontacts"] = community_networks.communitycontacts
     d["socialcontacts"]    = social_networks.socialcontacts
-    s = JSON.json(d)
+    #s = JSON.json(d)
+    s = JSON3.write(d)
     write(filename, s)
 end
 
 function load(filename::String)
     s = String(read(filename))
-    d = JSON.parse(s)
+    d = JSON3.read(s)
+    #d = JSON.parse(s)
     load_households!(d["households"])
     load_workplaces!(d["workplaces"])
     load_communitycontacts!(d["communitycontacts"])
